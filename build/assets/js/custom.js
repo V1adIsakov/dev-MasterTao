7ebc933283c6b7049ff2e26a2681013fb35d75f6
V1adIsakov <vlad2005isakov@gmail.com>
2024-01-15 22:04:04 +0200

    feat:Complete JS code in header

--- build/assets/js/custom.js
+++ build/assets/js/custom.js
@@ -1,2 +1,14 @@
   const burger = document.querySelector('.btn-burger');
+  const navigation = document.querySelector('.header__navigation');
+ 
+ 
+  console.log(burger);
+ 
+  function Menu() {
+    burger.classList.toggle('open')
+    navigation.classList.toggle('show');
+    console.log('click!)');
+  }
 
+
+  burger.addEventListener('click', Menu);
