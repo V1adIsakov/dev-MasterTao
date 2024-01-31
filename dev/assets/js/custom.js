f4375c8f33d8a1e3890731d744a3e75ae3e8bd19
V1adIsakov <vlad2005isakov@gmail.com>
2024-02-01 00:04:13 +0200

    feat: adventage desktop done

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -14,6 +14,35 @@ $(document).ready(function(){
     ]
   });
 
+  mobileOnlySlider("#slider-advantages", true, false, 1024);
+
+  function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+    
+    var slider = $($slidername);
+    var settings = {
+      mobileFirst: true,
+      dots: $dots,
+      arrows: $arrows,
+      responsive: [
+        {
+          breakpoint: $breakpoint,
+          settings: "unslick"
+        }
+      ]
+    };
+  
+    slider.slick(settings);
+  
+    $(window).on("resize", function () {
+      if ($(window).width() > $breakpoint) {
+        return;
+      }
+      if (!slider.hasClass("slick-initialized")) {
+        return slider.slick(settings);
+      }
+    });
+  } // Mobile Only Slider
+  
   const burger = document.querySelector('.btn-burger');
   const navigation = document.querySelector('.header__navigation');
  
@@ -63,7 +92,7 @@ $(document).ready(function(){
       const section = document.querySelectorAll('section')
       section.forEach(item => item.classList.add('charter'))
     }
-    
+
     document.querySelector('.home-page') ? addClassCharter() : null;
 
 });
